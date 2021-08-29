1eeeb1a699372fff211708a052227e9b645d1bfc
Parasite2142 <igi2131@gmail.com>
2021-08-29 20:04:25 +0300

    optimization

--- src/main/java/com/example/graalvmjacypt/GraalvmJacyptApplication.java
+++ src/main/java/com/example/graalvmjacypt/GraalvmJacyptApplication.java
@@ -7,14 +7,14 @@ import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfig
 import org.springframework.boot.autoconfigure.web.reactive.ReactiveWebServerFactoryAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.reactive.error.ErrorWebFluxAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.nativex.hint.*;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.ResourceHint;
+import org.springframework.nativex.hint.TypeHint;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -27,24 +27,17 @@ import java.util.Properties;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @NativeHint(
-        trigger = org.jasypt.normalization.Normalizer.class,
         types = @TypeHint(
                 types = {
                         java.text.Normalizer.class,
                         com.ibm.icu.text.Normalizer.class
                 }),
-        resources = {
-                @ResourceHint(patterns = "com/ibm/icu/impl/data/icudt67b/.*"),
-                @ResourceHint(patterns = "com/ibm/icu/impl/data/icudt67b.*"),
-        },
-        initialization = @InitializationHint(types = org.jasypt.normalization.Normalizer.class,
-                initTime = InitializationTime.BUILD)
+        resources = {@ResourceHint(patterns = "com/ibm/icu/impl/data/icudt67b.*")}
 )
 @SpringBootApplication
 public class GraalvmJacyptApplication {
     private static final Class<?>[] AUTO_CONFIG_CLASSES = {
             WebFluxAutoConfiguration.class,
-            WebMvcAutoConfiguration.class,
             ReactiveWebServerFactoryAutoConfiguration.class,
             HttpHandlerAutoConfiguration.class,
             ErrorWebFluxAutoConfiguration.class,
@@ -59,9 +52,8 @@ public class GraalvmJacyptApplication {
         return new SpringApplicationBuilder()
                 .sources(AUTO_CONFIG_CLASSES)
                 .initializers((GenericApplicationContext ctx) -> {
-                    final String playEncryptorKey = "playEncryptor";
-                    final String prodEncryptorKey = "productionEncryptor";
-
+                    final String playEncKey = "playEnc";
+                    final String prodEncKey = "productionEnc";
                     final Properties properties = new Properties();
                     try {
                         properties.load(GraalvmJacyptApplication.class
@@ -71,39 +63,38 @@ public class GraalvmJacyptApplication {
                         throw new RuntimeException(e);
                     }
 
-                    ctx.registerBean(RouterFunction.class,
-                            () -> RouterFunctions.resources("/**", new ClassPathResource("/")));
+                    ctx.registerBean(RouterFunction.class, () -> RouterFunctions.resources("/**", new ClassPathResource("/")));
 
-                    ctx.registerBean(prodEncryptorKey, BasicTextEncryptor.class, () -> {
-//                        Properties properties = ctx.getBean("appProps", Properties.class);
-                        BasicTextEncryptor encryptor = new BasicTextEncryptor();
-                        encryptor.setPassword(properties.getProperty("jacypt.prod.password"));
-                        return encryptor;
-                    });
+                    ctx.registerBean(prodEncKey, BasicTextEncryptor.class, () -> getEncryptor(properties.getProperty("jacypt.prod.password")));
 
-                    ctx.registerBean(playEncryptorKey, BasicTextEncryptor.class, () -> {
-//                        Properties properties = ctx.getBean("appProps", Properties.class);
-                        BasicTextEncryptor encryptor = new BasicTextEncryptor();
-                        encryptor.setPassword(properties.getProperty("jacypt.play.password"));
-                        return encryptor;
-                    });
+                    ctx.registerBean(playEncKey, BasicTextEncryptor.class, () -> getEncryptor(properties.getProperty("jacypt.play.password")));
 
                     ctx.registerBean(RouterFunction.class, () -> {
-                        BasicTextEncryptor productionEncryptor = ctx.getBean(prodEncryptorKey, BasicTextEncryptor.class);
-                        BasicTextEncryptor playEncryptor = ctx.getBean(playEncryptorKey, BasicTextEncryptor.class);
+                        BasicTextEncryptor productionEncryptor = ctx.getBean(prodEncKey, BasicTextEncryptor.class);
+                        BasicTextEncryptor playEncryptor = ctx.getBean(playEncKey, BasicTextEncryptor.class);
                         return route().POST("/api/v1/encrypt", serverRequest -> ServerResponse.ok().body(Mono.defer(() -> {
                                     String enc = serverRequest.queryParam("encType").orElse("").toLowerCase();
                                     String value = serverRequest.queryParam("value").
                                             orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "value is missing"));
 
-                                    if ("prod".equals(enc)) return Mono.just(playEncryptor.encrypt(value));
-                                    else if ("play".equals(enc)) return Mono.just(productionEncryptor.encrypt(value));
-                                    else return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unknown encType"));
-
+                                    switch (enc) {
+                                        case "play":
+                                            return Mono.just(playEncryptor.encrypt(value));
+                                        case "production":
+                                            return Mono.just(productionEncryptor.encrypt(value));
+                                        default:
+                                            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unknown encType"));
+                                    }
                                 }), String.class))
                                 .build();
                     });
                 })
                 .build();
+    }
+
+    private static BasicTextEncryptor getEncryptor(String password) {
+        BasicTextEncryptor encryptor = new BasicTextEncryptor();
+        encryptor.setPassword(password);
+        return encryptor;
     }
 }
